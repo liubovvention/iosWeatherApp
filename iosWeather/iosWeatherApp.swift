fd5c15a4c6af4d646bf27a6b5d865add413934af
liubovvention <liubov.kuskova@ventionteams.com>
2025-03-04 15:14:41 +0200

    Initial Commit

---
+++ iosWeather/iosWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosWeatherApp.swift
+//  iosWeather
+//
+//  Created by Liubov on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct iosWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iosWeatherTests/iosWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  iosWeatherTests.swift
+//  iosWeatherTests
+//
+//  Created by Liubov on 04/03/2025.
+//
+
+import Testing
+@testable import iosWeather
+
+struct iosWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
